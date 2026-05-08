e823c75aad8c21a692214c2ee8b0c5bd73e83812
John Doe <sacosa634@gmail.com>
2026-05-08 18:33:06 -0500

    first commit

---
+++ backend-salafinder/Models/Espacio.cs
@@ -0,0 +1,12 @@
+﻿namespace backend_salafinder.Models {
+    public class Espacio {
+        public string nombre_evento { get; set; }
+        public string tipo { get; set; }
+        public int capacidad { get; set; }
+        public string edificio { get; set; }
+        public string descripcion { get; set; }
+        public string[] recursos { get; set; }
+        public string[] programas_prioritarios { get; set; }
+        public bool requiere_aprobacion { get; set; } = false;
+    }
+}
