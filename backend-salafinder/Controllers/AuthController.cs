428ed5ee25ebfbbf6f577a3d1c4806969a504626
John Doe <sacosa634@gmail.com>
2026-05-16 19:59:20 -0500

    x

--- backend-salafinder/Controllers/AuthController.cs
+++ backend-salafinder/Controllers/AuthController.cs
@@ -1,52 +1,56 @@
 ﻿using backend_salafinder.Interfaces;
 using backend_salafinder.Models.DTO;
 using backend_salafinder.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 
-namespace backend_salafinder.Controllers
-{
+namespace backend_salafinder.Controllers {
     [ApiController]
     [Route("[controller]")]
-    public class AuthController : Controller
-    {
+    public class AuthController : Controller {
         private readonly IAuthService _auth_service;
 
-        public AuthController(IAuthService auth_service)
-        {
+        public AuthController(IAuthService auth_service) {
             _auth_service = auth_service;
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterDTO dto)
-        {
-            try
-            {
+        public async Task<IActionResult> Register([FromBody] RegisterDTO dto) {
+            try {
                 var response = await _auth_service.Register(dto);
                 return Ok(response);
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 return BadRequest(new { message = ex.Message });
             }
         }
 
         // POST /auth/login
         [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] LoginDTO dto)
-        {
-            try
-            {
+        public async Task<IActionResult> Login([FromBody] LoginDTO dto) {
+            try {
                 var response = await _auth_service.Login(dto);
                 return Ok(response);
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 return BadRequest(new { message = ex.Message });
             }
         }
 
-        public IActionResult Index()
-        {
+        [HttpPatch("cambiar-rol")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> CambiarRol([FromBody] CambiarRolDTO dto) {
+            try {
+                await _auth_service.CambiarRol(dto);
+                return Ok(new { message = $"Rol actualizado a {dto.nuevo_rol} correctamente." });
+            } catch (Exception e) {
+                return BadRequest(new { message = e.Message });
+            }
+        }
+
+        public IActionResult Index() {
             return View();
         }
     }
---
+++ backend-salafinder/Models/DTO/CambiarRolDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace backend_salafinder.Models.DTO {
+    public class CambiarRolDTO {
+        public Guid usuario_perfil_id { get; set; }
+        public string nuevo_rol { get; set; }
+    }
+}
