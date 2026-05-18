9ba673db758ef12637dd19652d1d4ab6a781cd47
John Doe <sacosa634@gmail.com>
2026-05-18 18:23:19 -0500

    FEAT: añadido servicio de registrar inasistencia (staff)

---
+++ backend-salafinder/Models/DTO/RegistrarNoShowDTO.cs
@@ -0,0 +1,5 @@
+﻿namespace backend_salafinder.Models.DTO {
+    public class RegistrarNoShowDTO {
+        public Guid id { get; set; }
+    }
+}
--- backend-salafinder/obj/Debug/net10.0/backend-salafinder.AssemblyInfo.cs
+++ backend-salafinder/obj/Debug/net10.0/backend-salafinder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-salafinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9693c4b6d1850a4fb79009f698a594a0e6341b01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78c1dffaafbb7f196f5649f01fb7c731e5abdb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-salafinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-salafinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
